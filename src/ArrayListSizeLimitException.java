fcbee6a11f4e761e3f94b46a515d5c901b8559bd
dany0k <zmaev.vsu@gmail.com>
2023-09-23 10:06:28 +0300

    Add exception

---
+++ src/ArrayListSizeLimitException.java
@@ -0,0 +1,73 @@
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+public class ArrayListSizeLimitException extends Exception {
+    private String message;
+
+    public ArrayListSizeLimitException() {
+        super();
+    }
+
+    public ArrayListSizeLimitException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "SizeLimitExceededException: " + message;
+    }
+
+    @Override
+    public String getLocalizedMessage() {
+        return super.getLocalizedMessage();
+    }
+
+    @Override
+    public synchronized Throwable getCause() {
+        return super.getCause();
+    }
+
+    @Override
+    public synchronized Throwable initCause(Throwable cause) {
+        return super.initCause(cause);
+    }
+
+    @Override
+    public void printStackTrace() {
+        super.printStackTrace();
+    }
+
+    @Override
+    public void printStackTrace(PrintStream s) {
+        super.printStackTrace(s);
+    }
+
+    @Override
+    public void printStackTrace(PrintWriter s) {
+        super.printStackTrace(s);
+    }
+
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return super.fillInStackTrace();
+    }
+
+    @Override
+    public StackTraceElement[] getStackTrace() {
+        return super.getStackTrace();
+    }
+
+    @Override
+    public void setStackTrace(StackTraceElement[] stackTrace) {
+        super.setStackTrace(stackTrace);
+    }
+}
